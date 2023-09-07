c5155ed55bfa59277da2f82a3a054a35df2d773b
Rick de Zwaan <58414967+Snowycodes@users.noreply.github.com>
2023-09-07 10:06:42 +0200

    changed function to return the value it generates
    The getComputerChoice function

--- script.js
+++ script.js
@@ -2,33 +2,40 @@
 
 // Function declaration getComputerChoice
 function getComputerChoice() {
+    let result = "";
+    // generate a random value, 1, 2, or 3. and put into variable
+    let randomValue = Math.floor((Math.random() * 3) + 1);
 
-// generate a random value, 1, 2, or 3. and put into variable
-let randomValue = Math.floor((Math.random()* 3)+1);
+
+    // Assign 1 to rock, 2 to paper, 3 to scissors.
+    // print the selected value in the console.
+    if (randomValue == 1) {
+
+        result = "ROCK";
+
+    } else if (randomValue == 2) {
+
+        result = "PAPER";
+
+    } else {
+
+        result = "SCISSORS";
+
+    }
 
 
-// Assign 1 to rock, 2 to paper, 3 to scissors.
-// print the selected value in the console.
-if(randomValue == 1){
-
-    console.log("Rock")
-
-} else if(randomValue == 2){
-
-    console.log("Paper")
-
-} else {
-
-    console.log("Scissors")
-
+    return result;
 }
 
 
-    return;
-  }
+console.log(getComputerChoice())
 
 
 
+
+// function playRound(playerSelection, computerSelection) {
+//    // your code here!
+// }
 
 
 
