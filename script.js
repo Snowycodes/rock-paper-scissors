d0be3922e12d3594d8880f38fbe67a36622b66a4
Rick de Zwaan <58414967+Snowycodes@users.noreply.github.com>
2023-09-07 09:29:17 +0200

    First part, function with output
    Returns R/P/S based on a pseudorandom value

---
+++ script.js
@@ -0,0 +1,34 @@
+// Pseudo code
+
+// Function declaration getComputerChoice
+function getComputerChoice() {
+
+// generate a random value, 1, 2, or 3. and put into variable
+let randomValue = Math.floor((Math.random()* 3)+1);
+
+
+// Assign 1 to rock, 2 to paper, 3 to scissors.
+// print the selected value in the console.
+if(randomValue == 1){
+
+    console.log("Rock")
+
+} else if(randomValue == 2){
+
+    console.log("Paper")
+
+} else {
+
+    console.log("Scissors")
+
+}
+
+
+    return;
+  }
+
+
+
+
+
+
