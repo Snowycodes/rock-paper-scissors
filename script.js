2691ed72c8cf82d3f063b3bb65252dc70e95b89d
Rick de Zwaan <58414967+Snowycodes@users.noreply.github.com>
2023-09-07 15:51:19 +0200

    Added a 5 round game function
    keeps track of the score and finishes after 5 non-draw
    rounds have been played.

--- script.js
+++ script.js
@@ -1,79 +1,140 @@
-// Pseudo code
+// Scoreboard global vars
+let playerScore = 0;
+let computerScore = 0;
+
+// run game
+game();
 
 // Function declaration getComputerChoice
 function getComputerChoice() {
-    let result = "";
-    // generate a random value, 1, 2, or 3. and put into variable
-    let randomValue = Math.floor((Math.random() * 3) + 1);
+	let result = "";
+	// generate a random value, 1, 2, or 3. and put into variable
+	let randomValue = Math.floor((Math.random() * 3) + 1);
 
-    // Assign 1 to rock, 2 to paper, 3 to scissors.
-    // print the selected value in the console.
-    if (randomValue == 1) {
+	// Assign 1 to rock, 2 to paper, 3 to scissors.
+	// print the selected value in the console.
+	if (randomValue == 1) {
 
-        result = "ROCK";
+		result = "ROCK";
 
-    } else if (randomValue == 2) {
+	} else if (randomValue == 2) {
 
-        result = "PAPER";
+		result = "PAPER";
 
-    } else {
+	} else {
 
-        result = "SCISSORS";
+		result = "SCISSORS";
 
-    }
+	}
 
-    return result;
+	return result;
 }
 
 // Function to collect input, makes it uppercase so the input is always uniform
 function playerSelection() {
 
-    let input = prompt("Please enter rock/paper/scissors to play").toUpperCase();
+     let input = prompt("Please enter rock/paper/scissors to play").toUpperCase();
 
-    return input;
+	// stub for testing. You can let the computer play vs. itself.
+	// input = getComputerChoice();
+	// --------- end stub
+
+	return input;
 }
 
 
+
+// function that handles logic of a round of play
 function playRound(playerSelection, computerSelection) {
 
-    console.log(`Computer used ${computerSelection} and player uses ${playerSelection}`);
+	let result = "";
 
-    if(playerSelection === computerSelection) {
-        
-        console.log("It is a draw!")
+	console.log(`Computer used ${computerSelection} and player uses ${playerSelection}`);
 
-    } else if(playerSelection === "ROCK" && computerSelection === "PAPER") {
+	if (playerSelection === computerSelection) {
 
-        console.log(` PAPER beats ROCK! You lose!`)
+		result = "DRAW";
+		console.log("It is a draw!")
 
-    } else if(playerSelection === "ROCK" && computerSelection === "SCISSORS") {
+	} else if (playerSelection === "ROCK" && computerSelection === "PAPER") {
 
-        console.log(`ROCK BEATS SCISSORS You win!`)
-        
-    } else if(playerSelection === "PAPER" && computerSelection === "SCISSORS") {
-        
-        console.log(`SCISSORS beats PAPER! You lose!`)
-       
-    }else if(playerSelection === "PAPER" && computerSelection === "ROCK") {
+		result = "Computer_WIN";
+		console.log(` PAPER beats ROCK! You lose!`)
 
-        console.log(`PAPER BEATS ROCK You win!`)
-     
-    }else if(playerSelection === "SCISSORS" && computerSelection === "ROCK") {
+	} else if (playerSelection === "ROCK" && computerSelection === "SCISSORS") {
 
-        console.log(`ROCK beats SCISSORS! You lose!`)
-       
-    } else if(playerSelection === "SCISSORS" && computerSelection === "PAPER" ) {
+		result = "Player_WIN";
+		console.log(`ROCK BEATS SCISSORS You win!`)
 
-        console.log(`SCISSORS beats PAPER You win!`)
-        
-    } else {
-        console.log("invalid player input")
-    }
-   
+	} else if (playerSelection === "PAPER" && computerSelection === "SCISSORS") {
+
+		result = "Computer_WIN";
+		console.log(`SCISSORS beats PAPER! You lose!`)
+
+	} else if (playerSelection === "PAPER" && computerSelection === "ROCK") {
+
+		result = "Player_WIN";
+		console.log(`PAPER beats ROCK You win!`)
+
+	} else if (playerSelection === "SCISSORS" && computerSelection === "ROCK") {
+
+		result = "Computer_WIN";
+		console.log(`ROCK beats SCISSORS! You lose!`)
+
+	} else if (playerSelection === "SCISSORS" && computerSelection === "PAPER") {
+
+		result = "Player_WIN";
+		console.log(`SCISSORS beats PAPER You win!`)
+
+	} else {
+		console.log("invalid player input")
+		result = "INVALID_INPUT"
+	}
+
+	return result;
 }
 
 
+// function that runs games until best out of 5 is reached, 
+// I decided not count draws and forfeit the round so you end up playing more than 5 rounds since drawing happens quite often in 5 tries.
+function game() {
 
-playRound(playerSelection(), getComputerChoice())
+	console.log();
 
 
+	while (parseInt(playerScore) + parseInt(computerScore) < 5) {
+
+		if (playRound(playerSelection(), getComputerChoice()) == "Player_WIN") {
+
+			playerScore++
+
+		} else if (playRound(playerSelection(), getComputerChoice()) == "Computer_WIN") {
+
+			computerScore++
+
+		} else {
+
+			// invalid input run again
+		}
+
+		if ((parseInt(playerScore) + parseInt(computerScore) < 5)) {
+
+			console.log(`the score is player ${playerScore} computer ${computerScore}`)
+
+		} else {
+
+			console.log(`the final score is player ${playerScore} computer ${computerScore}`);
+			if (parseInt(playerScore) > parseInt(computerScore)) {
+
+				console.log("Congratulations, you win!")
+
+			} else {
+
+				console.log(":( you lose!")
+
+			}
+		}
+
+	}
+
+}
